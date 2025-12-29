94cfeeef27303f362cb2c41ed54149692ce752fe
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-29 14:19:38 +0200

    Implement turn management and active player highlighting with state management and prop drilling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import GameBoard from "./components/GameBoard"
-import Player from "./components/Player"
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
+import Player from "./components/Player";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curentActivePlayer) => curentActivePlayer === "X" ? "O" : "X");
+    setGameTurns();
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === "X"} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
       </div>
     </main>
   )
